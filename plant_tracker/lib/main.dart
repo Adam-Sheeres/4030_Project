1edd28ad477a8ab826cb053bef857deb760a8ede
Adam <asheeres90@gmail.com>
2023-01-16 10:30:24 -0500

    Styled the AppBar

--- plant_tracker/lib/main.dart
+++ plant_tracker/lib/main.dart
@@ -4,8 +4,10 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text('Adam Sheeres-Paulicpulle'),
-      ),
+          title: Text('Adam Sheeres'),
+          backgroundColor: Color.fromARGB(255, 156, 232, 94),
+          titleTextStyle:
+              TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 22)),
       body: Center(child: Text('Hello World')),
     ),
   ));
