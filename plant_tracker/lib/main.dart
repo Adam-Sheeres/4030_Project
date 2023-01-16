c1340f4cd1679fb4ce3918914fee5427cb3edb56
Adam <asheeres90@gmail.com>
2023-01-16 11:04:06 -0500

    ex_01 finished

--- plant_tracker/lib/main.dart
+++ plant_tracker/lib/main.dart
@@ -3,12 +3,22 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(
-          title: Text('Adam Sheeres'),
-          backgroundColor: Color.fromARGB(255, 156, 232, 94),
-          titleTextStyle:
-              TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 22)),
-      body: Center(child: Text('Hello World')),
-    ),
+        appBar: AppBar(
+            title: Text('Adam Sheeres'),
+            backgroundColor: Color.fromARGB(255, 156, 232, 94),
+            titleTextStyle:
+                TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 22)),
+        body: Center(
+            child: Text(
+          'Hello World',
+          style: const TextStyle(
+              color: Color.fromARGB(255, 0, 0, 0), fontSize: 48),
+        )),
+        floatingActionButton: ElevatedButton(
+          onPressed: () {},
+          child: Text('Press me!'),
+          style: ElevatedButton.styleFrom(
+              primary: Colors.blue, onPrimary: Colors.white),
+        )),
   ));
 }
