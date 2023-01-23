5532dc5f253b85d75b31cbcaa0280d62f8e98e38
Adam <asheeres90@gmail.com>
2023-01-23 13:54:04 -0500

    update

--- plant_tracker/lib/main.dart
+++ plant_tracker/lib/main.dart
@@ -2,76 +2,78 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-List<String> daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+List<String> names = ['Riverside Park', 'The Boathouse Tea Room'];
+List<List<String>> things = [
+  ['Picnic', 'Playground', 'Hiking'],
+  ['Boating', 'Ice-Cream', 'Tea']
+];
+List<String> addresses = ['709 Woolwich St, Guelph', '116 Gordon St, Guelph'];
+List myImages = [
+  Image.asset('assets/images/riversidePark.png'),
+  Image.asset('assets/images/boathouseTearoom.png')
+];
+List iconList = [Icons.money_off_csred_rounded, Icons.attach_money];
 int dayOfWeek = -1;
 
 //Text(daysOfWeek[dayOfWeek])
-class getTaskRow extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    dayOfWeek++;
-    return Expanded(
-        child: Container(
-      color: Colors.white,
-      margin: EdgeInsets.all(5),
-      height: 75,
-      child: Align(
-        alignment: Alignment.centerLeft,
-        child: Container(
-            width: 50,
-            height: 50,
-            decoration:
-                BoxDecoration(color: Colors.orange, shape: BoxShape.circle),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[Text(daysOfWeek[dayOfWeek])],
-            )),
-      ),
-    ));
-  }
-}
+// class getTaskRow extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     dayOfWeek++;
+//     return Expanded(
+//         child: Container(
+//       color: Colors.white,
+//       margin: EdgeInsets.all(5),
+//       height: 75,
+//       child: Align(
+//         alignment: Alignment.centerLeft,
+//         child: Container(
+//             width: 50,
+//             height: 50,
+//             decoration:
+//                 BoxDecoration(color: Colors.orange, shape: BoxShape.circle),
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.center,
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: <Widget>[Text(daysOfWeek[dayOfWeek])],
+//             )),
+//       ),
+//     ));
+//   }
+// }
+
+/**TODO: 
+ * Create a 'card' for the two things
+ * align the 'aspects' about each of the cards 
+ * get that line showing up -> spacing? 
+ */
 
 class BodyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Column(children: [
-      for (int i = 0; i < 5; i++)
-        Row(
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Stack(children: [
-                  Container(
-                    height: 75,
-                    color: Colors.blue,
+      child: Column(children: [
+        for (int i = 0; i < 2; i++)
+          Container(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  //Vertical Alignment for the Card
+                  Text(names[i]),
+                  myImages[i],
+                  Row(
+                    children: [
+                      Text(things[i][0]),
+                      Text(things[i][1]),
+                      Text(things[i][2])
+                    ],
                   ),
-                  Positioned(
-                      top: 15,
-                      left: 5,
-                      child: Container(
-                          width: 50,
-                          height: 50,
-                          decoration: BoxDecoration(
-                              color: Colors.orange, shape: BoxShape.circle),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[Text(daysOfWeek[i])],
-                          ))),
-                  Positioned(
-                      right: 135,
-                      top: 20,
-                      child: Column(
-                          children: [Text("task one"), Text("task two")]))
-                ]),
-              ),
-            )
-          ],
-        )
-    ]));
+                  Text(addresses[i]),
+                  Icon(iconList[i])
+                ],
+              ))
+      ]),
+    );
   }
 }
 
